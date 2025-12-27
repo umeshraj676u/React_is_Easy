2a18d32bd1281c4feeb3597ca84d487d46681237
Umesh <umeshraj676u@gmail.com>
2025-12-27 18:15:05 +0530

    Create a Header Component from scratch using Functional Components with JSX, logo add, SearchBar in Middle, User icon right side

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-//React Element 
-const heading = (
-  <h1 className="head" tabIndex ="5">
-    Hello World from React using jsx!
-  </h1>
-);
 
-// React functional component
-const HeadingComponent = () => (
-  <div id="container">
-    {heading}
-    <h1 className="heading">Hello World from React using component! </h1>
-  </div>
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />); 
+/* Header Functional Component */
+const Header = () => {
+  return (
+    <header className="header">
+    
+      <div className="logo">MyApp</div>
+
+   
+      <div className="search">
+        <input type="text" placeholder="Search..." />
+      </div>
+
+     
+      <div className="user">👨‍🎓</div>
+    </header>
+  );
+};
+const App = () => {
+  return (
+    <Header/>
+  );
+};
+
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App/>);
